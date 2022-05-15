ba999fb5a3cdcdd060f091009ab30df06ab3374f
Wizetux <wizetux@gmail.com>
2022-05-15 11:58:47 -0700

    master: Add Mod-Tap key for LGUI/Space; Add configuration to increase hold time for Mod-Tap key

--- dvorak_iris/config.h
+++ dvorak_iris/config.h
@@ -34,3 +34,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+#define IGNORE_MOD_TAP_INTERRUPT
